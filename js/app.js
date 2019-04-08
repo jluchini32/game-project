e98222fb117469e418b2aadcc7d3b54a9b427d59
Joe Luchini <joeluchini@Joes-MacBook-Pro.local>
2019-04-08 15:14:37 -0600

    initial gameboard setup

---
+++ js/app.js
@@ -0,0 +1,7 @@
+console.log('up and running');
+
+// $('.toggle').on('click', function() {});
+// is equivalent of $('.toggle').click(function() {});
+$('.cell').on('click', function() {
+    $('id').toggleClass('show-list');
+    });
